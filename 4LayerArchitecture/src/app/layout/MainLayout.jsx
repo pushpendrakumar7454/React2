80882cbfed4c9c56cacbdda842cdf72688e3aca5
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-08-03 09:18:16 +0530

    4 layerArchitechure

---
+++ 4LayerArchitecture/src/app/layout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainLayout = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default MainLayout
+
---
+++ 4LayerArchitecture/src/routes/AppRoute.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { RouterProvider } from 'react-router'
+
+const AppRoute = () => {
+    const router=([
+
+    ])
+
+
+
+    return <RouterProvider router={router}/>
+}
+
+export default AppRoute
