08a46f1c66358a3c43536c582aea32ff7bc0f592
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-08-12 23:54:42 +0530

    infiniteScroling

--- infiniteScroling/src/App.jsx
+++ infiniteScroling/src/App.jsx
@@ -7,31 +7,40 @@ const App = () => {
 
    let limit=10
 
-  const {data,isPending } =
+  const {data,isPending,fetchNextPage,isFetching } =
   useInfiniteQuery({
     queryKey: ["products"],
     queryFn: ({pageParam})=>getProducts({limit,pageParam}),
     initialPageParam: 0,
     getNextPageParam:(lastPage,allPage)=>{
       let loadedData=allPage.length*limit
-      if(loadedData>=lastPage-1) return loadedData
+      if(loadedData<=lastPage.total) return loadedData
       return undefined
     }
     
   });
   if(isPending) return <h1>Pending</h1>
   console.log(data);
+
+  let allProducts=data.pages.flatMap((val)=>val.products)
   
 
 
 
   return (
-   <div className="max-w-7xl mx-auto px-6 py-10">
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-    {data?.pages[0]?.products.map((product) => (
+   <div  className={`max-w-7xl mx-auto px-6 py-10 transition-opacity duration-300 ${
+      isFetching ? "opacity-50" : "opacity-100"
+    }`}>
+    <div
+  
+     className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    {allProducts?.map((product) => (
       <ProductCard key={product.id} product={product} />
     ))}
   </div>
+ <div className='flex items-center justify-center p-4 text-xl'>
+   <button onClick={()=>fetchNextPage()} className='cursor-pointer active:scale-95'>Loading</button>
+ </div>
 </div>
   )
 }
