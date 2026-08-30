bc12b558cfaa6f5d393e96546de732f4da23c5fb
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-08-30 15:36:36 +0530

    story

--- instagram/src/features/story/ui/components/OtherStory.jsx
+++ instagram/src/features/story/ui/components/OtherStory.jsx
@@ -13,12 +13,24 @@ const OtherStory = ({ stories, onStoryClick }) => {
           >
             {/* Story Circle */}
             <div className="h-[68px] w-[68px] rounded-full bg-gradient-to-tr from-yellow-400 via-pink-500 to-purple-600 p-[3px]">
+
               <div className="h-full w-full rounded-full border-2 border-white bg-white p-[1px]">
-                <img
-                  src={story.image}
-                  alt={story.username}
-                  className="h-full w-full rounded-full object-cover"
-                />
+
+                {story.type?.startsWith("video/") ? (
+                  <video
+                    src={story.url}
+                    className="h-full w-full rounded-full object-cover"
+                    muted
+                    playsInline
+                  />
+                ) : (
+                  <img
+                    src={story.url}
+                    alt={story.username}
+                    className="h-full w-full rounded-full object-cover"
+                  />
+                )}
+
               </div>
             </div>
 
