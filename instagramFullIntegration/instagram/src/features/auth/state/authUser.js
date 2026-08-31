c687c17ab6fc8d29cd2040cd4c30523992c5ecd9
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-08-31 09:19:38 +0530

    register user

--- instagramFullIntegration/instagram/src/features/auth/state/authUser.js
+++ instagramFullIntegration/instagram/src/features/auth/state/authUser.js
@@ -1,25 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const useAuth = createSlice({
+const authSlice = createSlice({
     name: "auth",
+
     initialState: {
         user: null,
         isAuthenticated: false,
         isLoading: false
     },
+
     reducers: {
         addUser: (state, action) => {
-            state.user = action.payload
-            state.isAuthenticated = true
-            state.isLoading = false
+            state.user = action.payload;
+            state.isAuthenticated = true;
+            state.isLoading = false;
         },
+
         removeUser: (state) => {
-            state.user = null
-            state.isAuthenticated = false
-            state.isLoading = false
+            state.user = null;
+            state.isAuthenticated = false;
+            state.isLoading = false;
         }
     }
-})
+});
 
-export const { addUser, removeUser } = useAuth.actions
-export default useAuth.reducer
+export const { addUser, removeUser } = authSlice.actions;
+
+export default authSlice.reducer;
--- instagramFullIntegration/instagramBackend/src/app.js
+++ instagramFullIntegration/instagramBackend/src/app.js
@@ -1,10 +1,14 @@
 const express = require("express")
 const connectDb = require("./config/db")
 const userRouter = require("./router/user.router")
+const cors = require("cors")
 
 const app = express()
 
 app.use(express.json())
+app.use(cors({
+    origin: "*"
+}))
 connectDb()
 
 app.get('/', (req, res) => {
