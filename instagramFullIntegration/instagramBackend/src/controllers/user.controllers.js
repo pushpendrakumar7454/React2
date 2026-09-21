02b272da8b6a588d68b2fca2269b8e60f2771636
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-21 14:59:55 +0530

    conttroller

--- instagramFullIntegration/instagramBackend/src/controllers/user.controllers.js
+++ instagramFullIntegration/instagramBackend/src/controllers/user.controllers.js
@@ -157,3 +157,32 @@ export const authRefreshController=async(req,res)=>{
         })
     }
 }
+
+
+export const authMeController=async(req,res)=>{
+    try {
+          
+    
+        const {userId}=req.user
+
+        const user=await userModel.findById(userId)
+
+        return res.status(200).json({
+            message:"user find succefully",
+            data:{
+                user:{
+                    name:user.name,
+                    username:user.username,
+                    email:user.email,
+                    id:user._id
+                }
+            }
+        })
+
+
+    } catch (error) {
+        return res.status(500).json({
+            message:"internal server error"
+        })
+    }
+}
