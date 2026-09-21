44b41643488a30006e5f1ce5ef6da33430cf6d91
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-21 14:56:19 +0530

    middleware

---
+++ instagramFullIntegration/instagramBackend/src/middleware/user.middleware.js
@@ -0,0 +1,25 @@
+import { readAccessToken } from "../utils/auth"
+
+
+
+export const authenticate=async(req,res,next)=>{
+    try {
+    const accessToken= req.headers.authorization.split(" ")[1]
+
+    if(!accessToken){
+        return res.status(400).json({
+            message:"accessToken bot found"
+        })
+    }
+   
+    const decoded=readAccessToken(accessToken)
+
+    req.user=decoded
+    next()
+
+    } catch (error) {
+        return res.status(500).json({
+            message:"token expire"
+        })
+    }
+}
